e2571f305cffbb16fdb97ebaf3c8aae8d99d2fd7
German Montenegro <gnm3000@gmail.com>
2026-04-25 20:55:20 -0300

    fix test

--- features/support/world.ts
+++ features/support/world.ts
@@ -1,6 +1,8 @@
 import { IWorldOptions, World, setWorldConstructor } from '@cucumber/cucumber';
 import { Browser, BrowserContext, Page, chromium } from 'playwright';
 
+const baseURL = process.env.BASE_URL ?? 'http://127.0.0.1:4173';
+
 export class PWWorld extends World {
   browser!: Browser;
   context!: BrowserContext;
@@ -12,7 +14,7 @@ export class PWWorld extends World {
 
   async init() {
     this.browser = await chromium.launch();
-    this.context = await this.browser.newContext({ baseURL: 'http://127.0.0.1:4173' });
+    this.context = await this.browser.newContext({ baseURL });
     this.page = await this.context.newPage();
   }
 
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    baseURL: 'http://127.0.0.1:4173'
+    baseURL: process.env.BASE_URL ?? 'http://127.0.0.1:4173'
   }
 });
